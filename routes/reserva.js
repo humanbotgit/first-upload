e65e65d8f138b21adc97ed8500508a36d4837932
humanbotgit <jarancibiasedano@gmail.com>
2023-11-05 17:23:02 -0500

    un enter

--- routes/reserva.js
+++ routes/reserva.js
@@ -2,3 +2,10 @@ const express = require('express');
 const { body } = require('express-validator');
 const router = express.Router();
 const reservaController = require('../controllers/reserva')
+const auth = require('../middleware/auth');
+
+router.get('/docente/:DNI_Docente',reservaController.reservaDocente);
+router.post('/',[
+    auth,
+    body(Fecha_Reserva)
+])
