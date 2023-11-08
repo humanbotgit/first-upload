4a00c8dc90dd9cc514b93ac87e893eef3f70ab2f
humanbotgit <jarancibiasedano@gmail.com>
2023-11-08 13:43:25 -0500

    todo el const a un solo body

--- controllers/reserva.js
+++ controllers/reserva.js
@@ -17,12 +17,14 @@ exports.postReserva =async (req,res,next) => {
     if(!errors.isEmpty()){
         return res.status(422).json({ errors: errors.array() });
     }
-   const Fecha_Reserva= req.body.Fecha_Reserva;
-   const Inicio_Reserva = req.body.Inicio_Reserva;
-   const Fin_Reserva = req.body.Fin_Reserva;
-   const Cantidad_Licencias_Reservadas = req.body.Cantidad_Licencias_Reservadas;
-   const ID_Laboratorio = req.body.ID_Laboratorio;
-   const ID_Asignatura = req.body.ID_Asignatura;
+    const {
+        Fecha_Reserva,
+        Inicio_Reserva,
+        Fin_Reserva,
+        Cantidad_Licencias_Reservadas,
+        ID_Laboratorio,
+        ID_Asignatura
+    } = req.body;
     try{
         const reservaDetails ={
             Fecha_Reserva: Fecha_Reserva,
