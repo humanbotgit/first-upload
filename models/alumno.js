139d57f6185eb6c0531781bdbd96ee6ea027b958
humanbotgit <jarancibiasedano@gmail.com>
2023-11-14 16:25:12 -0500

    licencia y alumno aumento de modelos y bd de estudiantes

---
+++ models/alumno.js
@@ -0,0 +1,18 @@
+const db = require('../util/databaseestudiantes');
+class Alumno{
+    constructor(
+        idalumno,
+        dni_alumno,
+        nombres_alumno,
+        apellidos_alumno
+    ){
+        this.idalumno=idalumno;
+        this.dni_alumno=dni_alumno;
+        this.nombres_alumno=nombres_alumno;
+        this.apellidos_alumno=apellidos_alumno;
+    }
+    static selectAlumno(dni_alumno){
+        return db.execute('SELECT * FROM alumno WHERE dni_alumno = ?', [dni_alumno])
+    }
+}
+module.exports=Alumno;
---
+++ models/licencia.js
@@ -0,0 +1,10 @@
+const db = require('../util/database');
+class Licencia{
+    constructor(
+        ID_Licencia
+
+    ){
+        
+    }
+}
+module.exports = Reserva;
