add057b6b462b4c6c765e1276418ffb8619a4792
humanbotgit <jarancibiasedano@gmail.com>
2024-01-03 18:43:13 -0500

    CAMBIO A Alumno

--- models/alumno.js
+++ models/alumno.js
@@ -12,21 +12,14 @@ class Alumno{
         this.apellidos_alumno=apellidos_alumno;
     }
     static async selectAlumno(dni_alumno){
-        try {
+
             // Realizar el SELECT en la tabla alumno
-            const [alumnos, fields] = await db.execute('SELECT * FROM alumno WHERE dni_alumno = ?', [dni_alumno]);
+            await db.execute('SELECT * FROM alumno WHERE dni_alumno = ?', [dni_alumno]);
             // Verificar si se encontraron resultados en la tabla alumno
-            if (alumnos.length > 0) {
-              const alumno = alumnos[0];
               // Insertar un registro en la tabla registro con la hora actual y otros datos del alumno
               await db.execute('INSERT INTO registro (dni_alumno, hora_escaneo, nombres_alumno, apellidos_alumno) VALUES (?, NOW(), ?, ?)',
-                [alumno.dni_alumno, alumno.nombres_alumno, alumno.apellidos_alumno]);
-            }
+                [Alumno.dni_alumno, Alumno.nombres_alumno, Alumno.apellidos_alumno]);
     
-            return alumnos;
-          } catch (error) {
-            throw error;
-          }
     }
 }
 module.exports=Alumno;
