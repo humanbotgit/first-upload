7f0a00d46f7ae47309dc2022e0cf99dac92140f7
humanbotgit <121126539+humanbotgit@users.noreply.github.com>
2024-01-04 06:58:03 -0500

    Update alumno.js

--- controllers/alumno.js
+++ controllers/alumno.js
@@ -4,6 +4,7 @@ exports.selectAlumno=async(req,res,next)=>{
     try {
         const [[infoAlumno]]= await Alumno.selectAlumno(dni);
         res.status(200).json(infoAlumno);
+        console.log(dni);
     } catch (err) {
         if (!err.statusCode) {
             err.statusCode = 500;
