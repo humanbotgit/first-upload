61887aadf2d141e80fff5e15f48230544f743c1d
humanbotgit <jarancibiasedano@gmail.com>
2024-01-04 08:17:00 -0500

    just

--- models/alumno.js
+++ models/alumno.js
@@ -12,9 +12,7 @@ class Alumno{
         this.apellidos_alumno=apellidos_alumno;
     }
     static async selectAlumno(dni_alumno){
-            await db.execute('SELECT * FROM alumno WHERE dni_alumno = ?', [dni_alumno]);
-              await db.execute('INSERT INTO registro (dni_alumno, hora_escaneo, nombres_alumno, apellidos_alumno) VALUES (?, NOW(), ?, ?)',
-                [Alumno.dni_alumno, Alumno.nombres_alumno, Alumno.apellidos_alumno]);
+            return db.execute('SELECT * FROM alumno WHERE dni_alumno = ?', [dni_alumno]);
     }
 }
 module.exports=Alumno;
