0d3b1211e9a1cfb8e60c47960dcfaedb5a5e6b3c
humanbotgit <121126539+humanbotgit@users.noreply.github.com>
2024-01-04 06:46:53 -0500

    Update alumno.js

--- controllers/alumno.js
+++ controllers/alumno.js
@@ -1,19 +1,13 @@
 const Alumno = require('../models/alumno');
 exports.selectAlumno=async(req,res,next)=>{
     const dni = req.params.dni;
-
-  try {
-    // Llamar a la función para obtener información del alumno por su DNI
-    const [[infoAlumno]] = await Alumno.selectAlumno(dni);
-
-    // Llamar a la función para insertar un registro por DNI
-    await Alumno.insertRegistro(dni);
-
-    res.status(200).json({ infoAlumno, message: 'Registro insertado correctamente.' });
-  } catch (err) {
-    if (!err.statusCode) {
-      err.statusCode = 500;
+    try {
+        const [[infoAlumno]]= await Alumno.selectAlumno(dni);
+        res.status(200).json(infoAlumno);
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 500;
+        }
+        next(err);
     }
-    next(err);
-  }
 }
